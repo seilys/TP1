61cd41b828590b8b06bc7aff34b1263306ce827b
seilys <seilysir@gmail.com>
2022-09-20 16:33:58 +0200

    importamos los ficheros de la pr1.

---
+++ Pr1/src/tp1/p1/control/Controller.java
@@ -0,0 +1,62 @@
+package tp1.p1.control;
+
+import java.util.Scanner;
+
+import tp1.p1.logic.Game;
+import tp1.p1.view.GamePrinter;
+
+/**
+ * Accepts user input and coordinates the game execution logic.
+ *
+ */
+public class Controller {
+
+	private Game game;
+
+	private Scanner scanner;
+
+	private GamePrinter gamePrinter;
+
+	public Controller(Game game, Scanner scanner) {
+		this.game = game;
+		this.scanner = scanner;
+		this.gamePrinter = new GamePrinter(game);
+	}
+
+	/**
+	 * Draw / Paint the game.
+	 */
+	private void printGame() {
+		System.out.println(gamePrinter);
+	}
+
+	/**
+	 * Prints the final message once the match is finished.
+	 */
+	public void printEndMessage() {
+		System.out.println(gamePrinter.endMessage());
+	}
+
+	/**
+	 * Show prompt and request command.
+	 *
+	 * @return the player command as words
+	 */
+	private String[] prompt() {
+		System.out.print(Messages.PROMPT);
+		String line = scanner.nextLine();
+		String[] words = line.toLowerCase().trim().split("\\s+");
+
+		System.out.println(debug(line));
+
+		return words;
+	}
+
+	/**
+	 * Runs the game logic.
+	 */
+	public void run() {
+		// TODO fill your code
+	}
+
+}
